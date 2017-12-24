80aa2cea3457e8ee925ff86935d6dab8e6ea6ce1
huangp <373374101@qq.com>
2017-12-24 19:41:11 +0800

    整合shiro

--- src/main/java/com/stkj/common/SystemConfig.java
+++ src/main/java/com/stkj/common/SystemConfig.java
@@ -1,5 +1,14 @@
 package com.stkj.common;
 
 public class SystemConfig {
+	/**
+	 * 登录用户
+	 */
+	public final static String LOGIN_USER = "login_user";    
+	
+	/**
+	 * 验证码
+	 */
+	public final static String CAPTCHA_KEY = "captcha_key";
 
 }
